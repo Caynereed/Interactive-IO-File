166f8720290dc996e7092d9ce0f00d25c9f6bcfd
Caynereed <reed146@tritons.iowacentral.edu>
2023-09-28 08:52:10 -0500

    Initial Commit

---
+++ Open File/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Open File
+//
+//  Created by Cayne Reed on 9/28/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
